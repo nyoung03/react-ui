008bac9a4f5b5ff9b3db6afee96ddbb5af29d540
nyoung03 <vnfdlvdk37@gmail.com>
2022-08-18 23:06:18 +0900

    carousel 1차

---
+++ carousel/src/components/Carousel.tsx
@@ -0,0 +1,127 @@
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+
+const Base = styled.div``;
+
+const Container = styled.div`
+  position: relative;
+`;
+
+const ArrowButton = styled.button<{ pos: "left" | "right" }>`
+  position: absolute;
+  top: 50%;
+  z-index: 1;
+  padding: 8px 12px;
+  font-size: 48px;
+  font-weight: bold;
+  background-color: transparent;
+  color: #fff;
+  border: none;
+  margin: 0;
+  cursor: pointer;
+
+  ${({ pos }) => (pos === "left" ? "left: 0" : "right: 0")}
+`;
+
+const CarouselList = styled.ul`
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  overflow: hidden;
+`;
+
+const CarouselListItem = styled.li<{ activeIndex: number }>`
+  width: 100%;
+  flex: 1 0 100%;
+  transform: translateX(-${({ activeIndex }) => activeIndex * 100}%);
+  transition: 200ms ease;
+  > img {
+    width: 100%;
+    height: fit-content;
+  }
+`;
+
+const NavButton = styled.button<{ isActive?: boolean }>`
+  width: 4px;
+  height: 4px;
+  background-color: #000;
+  opacity: ${({ isActive }) => (isActive ? 0.3 : 0.1)};
+`;
+
+const NavItem = styled.li`
+  display: inline-block;
+`;
+
+const Nav = styled.ul`
+  list-style: none;
+  padding: 0;
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  ${NavItem} + ${NavItem} {
+    margin-left: 4px;
+  }
+`;
+
+const banners = [
+  "https://cdn.pixabay.com/photo/2018/06/03/14/17/sea-3450584_960_720.jpg",
+  "https://cdn.pixabay.com/photo/2016/11/21/17/09/animals-1846546_960_720.jpg",
+  "https://cdn.pixabay.com/photo/2013/01/10/14/18/starfish-74535_960_720.jpg",
+];
+
+const Carousel: React.FC = () => {
+  const [activeIndex, setActiveIndex] = useState<number>(0);
+  const [isFocuse, setIsFocuse] = useState(false);
+
+  const handleNext = () => {
+    setActiveIndex((prev) => (prev + 1) % banners.length);
+  };
+  const handlePrev = () => {
+    setActiveIndex((prev) => prev - 1 + banners.length);
+  };
+
+  const handleMouseEnter = () => setIsFocuse(true);
+  const handleMouseLeave = () => setIsFocuse(false);
+
+  const goTo = (idx: number) => {
+    setActiveIndex(idx);
+  };
+
+  useEffect(() => {
+    let intervalId: NodeJS.Timeout;
+
+    if (!isFocuse) {
+      intervalId = setInterval(handleNext, 3000);
+    }
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [isFocuse]);
+
+  return (
+    <Base onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+      <Container>
+        <ArrowButton pos="left" onClick={handlePrev}>{`<`}</ArrowButton>
+        <CarouselList>
+          {banners.map((banner, idx) => (
+            <CarouselListItem activeIndex={activeIndex} key={idx}>
+              <img src={banner} />
+            </CarouselListItem>
+          ))}
+        </CarouselList>
+        <ArrowButton pos="right" onClick={handleNext}>{`>`}</ArrowButton>
+      </Container>
+      <Nav>
+        {Array.from({ length: banners.length }).map((_, idx) => (
+          <NavItem key={idx} onClick={() => goTo(idx)}>
+            <NavButton isActive={activeIndex === idx} />
+          </NavItem>
+        ))}
+      </Nav>
+    </Base>
+  );
+};
+
+export default Carousel;
